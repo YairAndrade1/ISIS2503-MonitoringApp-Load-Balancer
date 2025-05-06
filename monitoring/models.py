1fb6ef891df15da08f09b102351b958f6ef6cbb6
Ximelocru <142061006+Ximelocru@users.noreply.github.com>
2025-05-06 13:40:48 -0500

    Update models.py

--- monitoring/models.py
+++ monitoring/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=100)
     history = models.TextField()
+    # Nuevo campo para la firma
+    digital_signature = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
