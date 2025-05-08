8bb67cec384e46c896f9d1930cf7b1e558996ae4
Yair Andrade <kidyarson@gmail.com>
2025-05-08 14:51:41 -0500

    undo

--- variables/logic/variable_logic.py
+++ variables/logic/variable_logic.py
@@ -8,9 +8,3 @@ def create_variable(form):
     measurement = form.save()
     measurement.save()
     return ()
-
-def get_variable_by_id(variable_id):
-    try:
-        return Variable.objects.get(id=variable_id)
-    except Variable.DoesNotExist:
-        return None
