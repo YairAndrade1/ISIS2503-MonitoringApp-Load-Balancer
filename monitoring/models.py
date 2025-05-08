746b87590d0ff1e0d1f16ab4fe79f3a7fdeff6a3
Ximelocru <142061006+Ximelocru@users.noreply.github.com>
2025-05-08 15:49:29 -0500

    Update models.py

--- monitoring/models.py
+++ monitoring/models.py
@@ -15,3 +15,13 @@ class Patient(models.Model):
         contenido = f'{self.name}|{self.history}'
         self.digital_signature = firmar_contenido(contenido)
         super().save(*args, **kwargs)
+
+         try:
+            requests.post('http://34.9.16.155:5000/verificar-integridad/', json={
+                "patient_id": self.id,
+                "name": self.name,
+                "history": self.history,
+                "digital_signature": self.digital_signature
+            })
+        except Exception as e:
+            print(f"[ERROR] No se pudo notificar al monitor: {e}")
