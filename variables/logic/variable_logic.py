cda0754b2b1c526e0ce82bae4d40cc599fa115b7
Yair Andrade <kidyarson@gmail.com>
2025-05-08 10:45:37 -0500

    Add get_variable_by_id function to retrieve a variable by its ID

--- variables/logic/variable_logic.py
+++ variables/logic/variable_logic.py
@@ -8,3 +8,9 @@ def create_variable(form):
     measurement = form.save()
     measurement.save()
     return ()
+
+def get_variable_by_id(variable_id):
+    try:
+        return Variable.objects.get(id=variable_id)
+    except Variable.DoesNotExist:
+        return None
